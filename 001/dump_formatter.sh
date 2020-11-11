2121d927bb61a1121b00ef62ede6e14cf934e603
serval7 <27344082+serval7@users.noreply.github.com>
2020-11-11 21:52:18 +0900

    second commit

--- 001/dump_formatter.sh
+++ 001/dump_formatter.sh
@@ -7,6 +7,7 @@ row_size=3
 
 cnt=0
 cnt_line=0
+line_str="first line"
 
 while read line
 do
@@ -14,7 +15,16 @@ do
     if [ ${cnt} -le ${skip_header} ] ;then
         continue
     fi
-    cnt_line=`expr $cnt_line + 1`
     rem=$((cnt_line % row_size))
-    echo "LINE $cnt $cnt_line: $line"
+    if [ ${rem} -eq 0 ] ; then
+        # new line
+        echo "${line_str}"
+        line_str=${line}
+    else
+        # append
+        line_str="${line_str},${line}"
+    fi
+    cnt_line=`expr $cnt_line + 1`
+    # echo "LINE $cnt $cnt_line: $line"
 done < ${dump_file}
+echo "${line_str}"
